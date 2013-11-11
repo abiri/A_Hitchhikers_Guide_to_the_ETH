631efa74372b9c1c96b337f2a36bb494847ce505
abiri <abiri@ethz.ch>
2013-11-11 18:40:24 +0100

    homo working
    finally did it :)

--- code/Simulation.m
+++ code/Simulation.m
@@ -13,24 +13,23 @@ agents = apti * time_span / dt;
 
 % initialize variables and preallocate memory (for maximized speed)
 total = 0;
-total_temp = 1:time_span/dt;
-poly_temp  = 1:time_span/dt;
-tram_temp  = 1:time_span/dt;
-walk_temp  = 1:time_span/dt;
+total_temp = zeros(time_span/dt,1);
+poly_temp  = zeros(time_span/dt,1);
+tram_temp  = zeros(time_span/dt,1);
+walk_temp  = zeros(time_span/dt,1);
 previous = 1; % index of maximal block of agents that has already been progressed
 
-poly_capacity = 80;
-tram_capacity = 120;
+poly_capacity = 40;
+tram_capacity = 50;
 walk_capacity = 10000; % atm infinite
 
 for i = 1:(time_span/dt); % loops through all time intervals
-    
     
     while ( M(previous,1) < 0)  % skips agents that already progressed through the entire system,  Path "-1" == done
         previous = previous + 1;
     end
     
-    for k = previous:(apti*(i-1))  % loops through previous agents
+    for k = previous:(apti*(i-1))  % loops through "old" agents (skips previous ones)
 
         % update old agents
 
@@ -38,36 +37,39 @@ for i = 1:(time_span/dt); % loops through all time intervals
 
         switch M(k,1)
             case 1
-                poly_temp(i) = poly_temp(i) + 1;
                 if ( M(k,2) > time_poly )
                     M(k,1) = -1;
+                else
+                    poly_temp(i) = poly_temp(i) + 1;
                 end
             case 2
-                tram_temp(i) = tram_temp(i) + 1;
                 if ( M(k,2) > time_tram )
                     M(k,1) = -1;
+                else
+                    tram_temp(i) = tram_temp(i) + 1;
                 end
             case 3
-                walk_temp(i) = walk_temp(i) + 1;
                 if ( M(k,2) > time_walk )
                     M(k,1) = -1;
+                else
+                    walk_temp(i) = walk_temp(i) + 1;
                 end
         end
     end
 
 
-    for k = (apti*(i-1) + 1):(apti*i) % loops through new agents at the end of stack
+    for j = (apti*(i-1) + 1):(apti*i) % loops through new agents at the end of stack
 
         % evaluate situation and position new agents
         
         if ( poly_temp(i) < poly_capacity)
-           M(k,1) = 1;
+           M(j,1) = 1;
            poly_temp(i) = poly_temp(i) + 1;
         elseif ( tram_temp(i) < tram_capacity)
-           M(k,1) = 2;
+           M(j,1) = 2;
            tram_temp(i) = tram_temp(i) + 1;
         else
-           M(k,1) = 3;
+           M(j,1) = 3;
            walk_temp(i) = walk_temp(i) + 1;
         end     
     end
